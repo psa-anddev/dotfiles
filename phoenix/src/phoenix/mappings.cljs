c53ff3117b155f705af88b9b4128d2214bf632c0
Pablo Sánchez <pablo.sanchez.alonso@gmail.com>
2019-01-29 22:23:30 +0000

    Fixed moving to screens.

--- phoenix/src/phoenix/mappings.cljs
+++ phoenix/src/phoenix/mappings.cljs
@@ -5,12 +5,12 @@
 (defn move-window-to-next-screen []
   "Moves the active window to the screen after the one the window is in"
   (windows/move-to-screen (windows/get-active)
-                          (screens/get-next (.screen (windows/get-active)))))
+                          (.next (.screen (windows/get-active)))))
 
 (defn move-window-to-previous-screen []
   "Moves the active window to the screen before the one the window is in"
   (windows/move-to-screen (windows/get-active)
-                          (screens/get-previous (.screen (windows/get-active)))))
+                          (.previous (.screen (windows/get-active)))))
 
 (defn current-shortcuts []
   "Shows a dialog with the current shortcuts"
