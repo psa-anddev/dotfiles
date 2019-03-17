f046d6e0ea0121aa6f8ab872978f7bee263e2762
Pablo Sánchez <pablo.sanchez.alonso@gmail.com>
2019-03-17 11:05:05 +0000

    Sorted the opening of two windows when launching a terminal app when opening it when Iterm is closed.

--- .scripts/iterm.sh
+++ .scripts/iterm.sh
@@ -1,19 +1,37 @@
 osascript <<EOF
 tell application "iTerm"
-	set new_window to (create window with default profile)
-	set cSession to current session of new_window
-	tell new_window
-		tell cSession
-			write text "source ~/.iterm2_shell_integration.zsh"
-			write text "$@"
-			repeat
-				delay 0.1
-				set isdone to is at shell prompt
-				if isdone then exit repeat
-			end repeat
-			close new_window
+	if it is running
+		set new_window to (create window with default profile)
+		set cSession to current session of new_window
+		tell new_window
+			tell cSession
+				write text "source ~/.iterm2_shell_integration.zsh"
+				write text "$@"
+				repeat
+					delay 0.1
+					set isdone to is at shell prompt
+					if isdone then exit repeat
+				end repeat
+				close new_window
+			end tell
 		end tell
-	end tell
+	else
+		activate
+		set new_window to current window
+		set cSession to current session of new_window
+		tell new_window
+			tell cSession
+				write text "source ~/.iterm2_shell_integration.zsh"
+				write text "$@"
+				repeat
+					delay 0.1
+					set isdone to is at shell prompt
+					if isdone then exit repeat
+				end repeat
+				close new_window
+			end tell
+		end tell
+	end if
 end tell
 EOF
 
