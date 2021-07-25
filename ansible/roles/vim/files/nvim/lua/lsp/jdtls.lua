069bd70e33692275c0585f4adc164240678bcfd4
Pablo Sánchez Alonso <pablo.sanchez.alonso@gmail.com>
2021-07-25 15:30:41 +0100

    Changed mappings configuration in order to use which key

--- ansible/roles/vim/files/nvim/lua/lsp/jdtls.lua
+++ ansible/roles/vim/files/nvim/lua/lsp/jdtls.lua
@@ -3,7 +3,6 @@ local M = {}
 function M.setup()
     local on_attach = function (client, bufnr)
         require 'jdtls.setup'.add_commands()
-        local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
         local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
         buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -23,27 +22,42 @@ function M.setup()
         })
 
         -- Mappings.
-        local opts = { noremap=true, silent=true }
+        local wk = require('which-key')
 
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        buf_set_keymap('n', '<leader>lgD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-        buf_set_keymap('n', '<leader>lgd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-        buf_set_keymap('n', 'K', '<Cmd>lua require(\'lspsaga.hover\').render_hover_doc()<CR>', opts)
-        buf_set_keymap('n', '<leader>lgi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-        buf_set_keymap('n', '<leader>lk', '<cmd>lua require(\'lspsaga.signaturehelp\').signature_help()<CR>', opts)
-        buf_set_keymap('n', '<leader>lwa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-        buf_set_keymap('n', '<leader>lwr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-        buf_set_keymap('n', '<leader>lwl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-        buf_set_keymap('n', '<leader>lt', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-        buf_set_keymap('n', '<leader>lrr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-        buf_set_keymap('n', '<leader>lrq', '<cmd>lua require("jdtls").code_action()<CR>', opts)
-        buf_set_keymap('n', '<Leader>lgr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-        buf_set_keymap('n', '<leader>lds', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-        buf_set_keymap('n', '<leader>ldp', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-        buf_set_keymap('n', '<leader>ldn', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-        buf_set_keymap('n', '<leader>ldl', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-        buf_set_keymap("n", "<leader>l=", "<cmd>Format<CR>", opts)
-
+        wk.register({ K = { function() require('lspsaga.hover').render_hover_doc() end, 'hover doc' }}, {})
+        wk.register({
+            l = {
+                name = 'lsp',
+                k = { function() require('lspsaga.signaturehelp').signature_help() end, 'signature help' },
+                [ "=" ] = { "<cmd>Format<CR>", "reformat" },
+                t = { function () vim.lsp.buf.type_definition() end, 'type definition' },
+                g = {
+                    name = "go to",
+                    D = { function () vim.lsp.buf.declaration() end, 'declaration' },
+                    d = {  function () vim.lsp.buf.definition() end, 'definition' },
+                    i = {  function () vim.lsp.buf.implementation() end, 'implementation' },
+                    r = {  function () vim.lsp.buf.references() end, 'references' }
+                },
+                w = {
+                    name = "workspace",
+                    a = {  function () vim.lsp.buf.add_workspace_folder() end, 'add folder' },
+                    r = {  function () vim.lsp.buf.remove_workspace_folder() end, 'remove folder' },
+                    l = { function () print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, 'list folders' }
+                },
+                r = {
+                    name = "refactor",
+                    r = { function () vim.lsp.buf.rename() end, 'rename' },
+                    q = { function () require('jdtls').code_action() end, 'code actions' }
+                },
+                d = {
+                    name = "diagnostics",
+                    s = { function () vim.lsp.diagnostic.show_line_diagnostics() end, 'show line' },
+                    p = { function () vim.lsp.diagnostic.goto_prev() end, 'go to previous' },
+                    n = {function () vim.lsp.diagnostic.goto_next() end, 'go to next' },
+                    l = {function () vim.lsp.diagnostic.set_loclist() end, 'set local list' },
+                }
+            }
+        }, { prefix = "<leader>"})
     end
     local config = {
         flags = {
