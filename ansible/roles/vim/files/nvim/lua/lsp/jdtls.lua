1f4a229fc0768c517c8b5bb3c1979acf4ed6594e
Pablo Sánchez Alonso <pablo.sanchez.alonso@gmail.com>
2021-07-25 21:42:46 +0100

    Making code actions appear in telescope for jdtls

--- ansible/roles/vim/files/nvim/lua/lsp/jdtls.lua
+++ ansible/roles/vim/files/nvim/lua/lsp/jdtls.lua
@@ -79,4 +79,36 @@ function M.setup()
     require('jdtls').start_or_attach(config)
 end
 
+local finders = require'telescope.finders'
+local sorters = require'telescope.sorters'
+local actions = require'telescope.actions'
+local pickers = require'telescope.pickers'
+require('jdtls.ui').pick_one_async = function(items, prompt, label_fn, cb)
+    local opts = {}
+    pickers.new(opts, {
+        prompt_title = prompt,
+        finder    = finders.new_table {
+            results = items,
+            entry_maker = function(entry)
+                return {
+                    value = entry,
+                    display = label_fn(entry),
+                    ordinal = label_fn(entry),
+                }
+            end,
+        },
+        sorter = sorters.get_generic_fuzzy_sorter(),
+        attach_mappings = function(prompt_bufnr)
+            actions.select_default:replace(function()
+                local selection = actions.get_selected_entry(prompt_bufnr)
+                actions.close(prompt_bufnr)
+
+                cb(selection.value)
+            end)
+
+            return true
+        end,
+    }):find()
+end
+
 return M
